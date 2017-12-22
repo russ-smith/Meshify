7a1b651fe1c1a3c672e2fe4fd489d5c416df7752
Russ Smith <russellsmith83@gmail.com>
2017-12-22 17:00:43 +0800

    Revert "testing"
    This reverts commit 2cc5190019b9ada6f8787e5c89b26dcb19e21d95.

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -14,7 +14,6 @@ void ofApp::setup(){
 	renderShader.load("DrawingShaders/phong2lights");
 	cam.setDistance(10);
 	cam.setNearClip(0.5);
-	//testing
 }
 
 //--------------------------------------------------------------
